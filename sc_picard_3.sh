e5c89c879b401d740e7ee751cc537e6a8ae167f4
niklaslang <niklasjonathanlang@gmail.com>
2019-07-03 13:04:08 +0200

    commit #1

---
+++ sc_picard_3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#presets
+
+#SBATCH --ntasks=8
+#SBATCH --mem=20000
+
+#picard - presets
+
+inDir=/PATH/TO/results/picard/picard_2
+
+outDir=/PATH/TO/results/picard/picard_3
+
+mkdir $outDir
+
+#picard_3 - build BAM index
+
+#creates an index file for the input BAM file that allows quick look-up of data in the BAM file
+
+java -jar /PATH/TO/picard.jar BuildBamIndex \
+I=$inDir/$file \
+TMP_DIR=./picard/
